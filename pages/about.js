21ea876ca9a3b312b48da885adb59b61f0e6e39d
Xin Yin <xinyin322@gmail.com>
2023-06-10 17:07:51 +0800

    Completed

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import styles from '../styles/About.module.css'
+
+function About(){
+    return (<><div>
+        <h1 className={styles.highlight}>About</h1>
+    </div></>)
+}
+
+export default About;
